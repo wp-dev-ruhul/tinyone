78e4fbf2ac8dc6a053e80ece9104783980a2b301
Ruhul Amin <100131957+wp-dev-ruhul@users.noreply.github.com>
2024-11-03 19:43:25 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// hero carusal js part start
+$(document).ready(function() {
+    $("#hero-1").owlCarousel({
+
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    
+                }
+            }
+
+    })
+  });
+
+
+
+// hero carusal js part End
